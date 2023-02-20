55e30a44c5f665541593d7ba232dbd412e42e4fc
Jiaqi7777 <jg851@cam.ac.uk>
2023-02-20 23:12:30 +0000

    error correction

--- QLearning.py
+++ QLearning.py
@@ -1,8 +1,8 @@
 import numpy as np
 from utils import *
-def DynamicProgramming(V, A, S, env, thresh = 0.2, gamma=0.95):
+def DynamicProgramming(Q, A, S, env, thresh = 1e-4, gamma=0.95):
     loop = 0
-    delta = thresh + 0.00000001
+    delta = thresh + 0.01
     pi = np.zeros(V.shape)
     while delta > thresh:
         loop += 1
@@ -10,24 +10,19 @@ def DynamicProgramming(V, A, S, env, thresh = 0.2, gamma=0.95):
         '''1-d state'''
         delta = 0.0001
         for s in S:
-            pre_v = V[s]
-            # print('prev', s, pre_v)
-            qsa = []
             for a in A:
+                pre_q = Q[s + (a,) ]
                 s1, r, done, _ = env.step(a, s)
-                qsa.append(r + gamma * V[s1])
-            pi[s] = np.argmax(qsa)
-            new_v = max(qsa)
-            # print('newv',new_v)
-            V[s] = new_v
-            delta = max(delta, abs(pre_v - new_v))
-        print('Value', V, delta)
+                new_q = Q[s + (a,) ] = r + gamma * max(Q[s1])
+                delta = max(delta, abs(pre_q - new_q))
+            
+    print('Q', np.round(Q, 3), delta)
     print(f'Converged with loop {loop}')
-    print('Value', V)
-    print('Policy:', pi)
+    print('Value', np.max(Q, axis=-1))
+    print('Policy:', np.argmax(Q, axis=-1))
     return pi
 
-def QLearning(Q, env, n_episodes=10, horizon=40, gamma=0.95):
+def QLearning(Q, env, n_episodes=10, horizon=50, gamma=0.95, epsilon=0.4):
     # while delta > thresh:
     #     loop += 1
     r_all_episodes = []
@@ -35,12 +30,12 @@ def QLearning(Q, env, n_episodes=10, horizon=40, gamma=0.95):
         R = 0
         s0, _ = env.reset()
         for t in range(horizon):
-            a = np.argmax(Q[s0])
+            a = np.argmax(Q[s0]) if np.random.uniform(0, 1) > epsilon else random.choice(range(env.action_space.n))
             s1, r, done, _ = env.step(a)
             R += r
             Q[s0][a] = r + gamma * max(Q[s1])
-            if done:
-                print('Done in ', t + 1, 'steps')
+            if False:#done:
+                # print('Done in ', t + 1, 'steps')
                 break
             s0 = s1
             
@@ -49,7 +44,8 @@ def QLearning(Q, env, n_episodes=10, horizon=40, gamma=0.95):
     pi = np.argmax(Q, axis=-1)
     # print('Value', V)
     print('Policy:', pi)
-    plot_return_vs_episodes(r_all_episodes, smooth=10)
+    # plot_return_vs_episodes(r_all_episodes, smooth=10)
+    print(np.round(Q,3))
     return pi, Q
     
 if __name__ == '__main__':
@@ -66,9 +62,14 @@ if __name__ == '__main__':
     env = GridWorld((3,4), obstacles=True)
 
     V = np.ones(env.n_cell)/env.observation_space.n
-    Q = np.ones(shape=(env.n_cell + (env.action_space.n, )))
-    print('Q table with shape', Q.shape, Q)
+    Q = np.ones(shape=(env.n_cell + (env.action_space.n, )))/env.observation_space.n / env.action_space.n
+    print('Q table with shape', Q.shape)
     # S = range(env.n_cell)
-    # A = range(env.action_space.n)
+    A = range(env.action_space.n)
     # DynamicProgramming(V, A, S, env)
-    pi = QLearning(Q, env, n_episodes=20, horizon=40)
+    # pi = QLearning(Q, env, n_episodes=20, horizon=150)
+    S = []
+    for i in range(env.n_cell[0]):
+        for j in range(env.n_cell[1]):
+            S.append((i,j)) 
+    DynamicProgramming(Q, A, S, env)
